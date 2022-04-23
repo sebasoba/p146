e1e86afaf083e8fbf7b9d190e1b714882186356f
sebasoba <76491101+sebasoba@users.noreply.github.com>
2022-04-23 00:09:09 -0700

    Add files via upload

---
+++ p146.js
@@ -0,0 +1,33 @@
+<html>
+<head>
+<script src="https://aframe.io/releases/0.6.0/aframe.min.js"> </script> 
+</head>
+<body>
+
+<a-scene>
+<a-sphere>
+    color="#000"
+    radius="0.15"
+    position="0 0 0"
+
+</a-sphere>
+
+<a-cube>
+    color="#000"
+    height="1"
+    width="1"
+    position="0 1 0"
+
+</a-cube>
+
+<a-triangle>
+    color="#000"
+    height="0.5"
+    width="0.5"
+    position="0 2 0"
+
+</a-triangle>
+</a-scene>
+</body>
+</html>
+
